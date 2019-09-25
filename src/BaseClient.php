9671348c2d5ffc31e342d397d0fa0fa4327683bd
Krot Eval <38257723+4n70w4@users.noreply.github.com>
2019-09-25 15:25:22 +0300

    declare as protected \BaseClient\BaseClient::exceptions()

--- src/BaseClient.php
+++ src/BaseClient.php
@@ -66,7 +66,7 @@ abstract class BaseClient {
      * @param ResponseInterface $response
      * @param string|null $content
      */
-    abstract function exceptions(ResponseInterface $response, string $content = null) : void;
+    abstract protected function exceptions(ResponseInterface $response, string $content = null) : void;
 
 
 
